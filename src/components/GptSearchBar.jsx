b8af2d1f78b0b77442952174b7d46a0895e3fe61
abu-hanzla <hanzla611@gmail.com>
2023-08-31 12:46:08 +0530

    gitignore

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,13 +1,32 @@
+import { useRef } from "react";
 import lang from "../utils/languageConstants";
 import { useSelector } from "react-redux";
+import openai from "../utils/openAI";
 
 function GptSearchBar() {
   const language = useSelector((store) => store.config.lang);
-  console.log(language);
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async () => {
+    const gptQuery =
+      "Act as a movie recommendation system and suggests some movies for the query:" +
+      searchText.current.value +
+      ". only give me names of 5 movies, comma separated like the example given ahead.Example Result: Transformers, Avatar, Jurrasic Park, Avengers, Hobbits";
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: gptQuery }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(gptResults.choices);
+  };
+
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className=" bg-black w-1/2 grid grid-cols-12">
+      <form
+        className=" bg-black w-1/2 grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           className="p-4 m-4 col-span-9"
           type="text"
           placeholder={
@@ -18,7 +37,10 @@ function GptSearchBar() {
               : lang.spanish.gptSearchPlaceholder
           }
         />
-        <button className="py-2 col-span-3 m-4 px-4 bg-red-700 text-white rounded-lg font-bold text-xl">
+        <button
+          onClick={handleGptSearchClick}
+          className="py-2 col-span-3 m-4 px-4 bg-red-700 text-white rounded-lg font-bold text-xl"
+        >
           {language === "en"
             ? lang.en.search
             : language === "hindi"
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -8,7 +8,7 @@ export const API_OPTIONS = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzA0MWRjZmQ3NzdhZTc5MjljNDExOTNiYjI3NjUxZCIsInN1YiI6IjY0ZWVjZTRiMjcxNjcxMDExZGUxMjZiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aIPO2OE89otvasr4pwBSRx-vLsLnt3oFPTP-8-7zb8w",
+      "Bearer" + process.env.REACT_APP_TMDB_KEY,
   },
 };
 
@@ -17,9 +17,12 @@ export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-
 export const SUPPORTED_LANGUAGES = [
-  {identifier:"en", name:"English"},
-  {identifier:"hindi", name:"Hindi"},
-  {identifier:"spanish", name:"Spanish"},
-]
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
+
+// export const OPENAI_KEY = 
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY
