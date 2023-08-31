986f7588af077aa9e9e2f20a3323cc83d0bd4937
abu-hanzla <hanzla611@gmail.com>
2023-08-31 11:33:27 +0530

    multilangauge

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import lang from '../utils/languageConstants'
+import lang from "../utils/languageConstants";
+import { useSelector } from "react-redux";
 
 function GptSearchBar() {
+  const language = useSelector((store) => store.config.lang);
+  console.log(language);
   return (
-    <div className='pt-[10%] flex justify-center'>
-    <form className=' bg-black w-1/2 grid grid-cols-12'>
-        <input className='p-4 m-4 col-span-9' type='text' placeholder={lang.hindi.gptSearchPlaceholder} />
-        <button className='py-2 col-span-3 m-4 px-4 bg-red-700 text-white rounded-lg font-bold text-xl'>{lang.hindi.search}</button>
-    </form>
+    <div className="pt-[10%] flex justify-center">
+      <form className=" bg-black w-1/2 grid grid-cols-12">
+        <input
+          className="p-4 m-4 col-span-9"
+          type="text"
+          placeholder={
+            language === "en"
+              ? lang.en.gptSearchPlaceholder
+              : language === "hindi"
+              ? lang.hindi.gptSearchPlaceholder
+              : lang.spanish.gptSearchPlaceholder
+          }
+        />
+        <button className="py-2 col-span-3 m-4 px-4 bg-red-700 text-white rounded-lg font-bold text-xl">
+          {language === "en"
+            ? lang.en.search
+            : language === "hindi"
+            ? lang.hindi.search
+            : lang.spanish.search}
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default GptSearchBar
+export default GptSearchBar;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,14 @@ import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 function Header() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const gptSearch = useSelector((store) => store.gpt.showGptSearch);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {})
@@ -46,21 +49,32 @@ function Header() {
     dispatch(toggleGptSearchView());
   };
 
+  const handleLangChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
       <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex items-center">
-          <select className="p-2 m-2 bg-gray-900 text-white">
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>🌎{lang.name}</option>
-            ))}
-          </select>
+          {gptSearch && (
+            <select
+              className="p-2 px-4 m-2 bg-gray-900 text-white"
+              onChange={handleLangChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  🌎{lang.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             className="py-2 px-4 m-2 font-bold bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearchClick}
           >
-            GPT Search
+            {gptSearch ? "Home" : "GPT Search"}
           </button>
           <img
             className="w-28 h-28 mr-2 p-4 rounded-full"
